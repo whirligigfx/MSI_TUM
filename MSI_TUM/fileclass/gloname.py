0fdb094da3cdec96ed92a073d53bd03c3a608667
whirligig <378272876@qq.com>
2017-04-15 11:38:43 +0800

    Signed-off-by: whirligig <378272876@qq.com>

---
+++ MSI_TUM/fileclass/gloname.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+COUNT = 0
+DELTA = 10
+LEN_READS = 90
+REF_INIT = 1000
+REF_PLUS = 500
+ITIE_TIME = 30
+MSI_INIT = 1.0
---
+++ MSI_TUM/fileclass/run.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+python ./maindetect.py HG1879_21sort_NoNameError.sam HG1879_21sort_NoNameError_90_1000_500.txt
+
+
+
